2323955a768e1b923245364e7f9d73730a42000a
Tsubasa Kudo <tsubasa.kudo@andco.group>
2019-03-18 09:55:54 +0900

    initial commit

--- dbconnect.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $dsn = 'mysql:dbname=booklikes;host=localhost';
-    $user = 'root';
-    $password='';
-    $dbh = new PDO($dsn, $user, $password);
-    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $dbh->query('SET NAMES utf8');
-
-?>
