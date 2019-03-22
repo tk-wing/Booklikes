0917c27de714cbb00c5f81c5aa1b39eb5723719a
Tsubasa Kudo <tsubasa.kudo@andco.group>
2019-03-22 18:12:00 +0900

    プロパティの追加($method, $path)

--- core/Action.php
+++ core/Action.php
@@ -3,10 +3,15 @@
 namespace Core;
 
 class Action{
+    public $mathod;
+    public $path;
     public $class;
     public $action;
+    public $query;
 
-    public function __construct($class, $action){
+    public function __construct($method, $path, $class, $action){
+        $this->method = $method;
+        $this->path = $path;
         $this->class = $class;
         $this->action = $action;
     }
