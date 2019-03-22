1dc4247c49606dcec9f5f5564ec1921e406c3512
Tsubasa Kudo <tsubasa.kudo@andco.group>
2019-03-22 18:08:18 +0900

    引数のクラスをapp → routerへ変更 　

--- app/Router.php
+++ app/Router.php
@@ -3,25 +3,22 @@
 namespace App;
 
 use Core\Response;
-use Core\Application;
 
 class Router
 {
-    public function web(Application $app)
+    public function web(\Core\Router $router)
     {
-        $app->get('/signup', Controllers\SignupController::class, 'show');
-        $app->post('/signup', Controllers\SignupController::class, 'register');
-        $app->get('/login', function () {
-            return Response::json(['hoge' => 'piyo']);
-        });
-        $app->get('/login', Controllers\LoginController::class, 'show');
-        $app->post('/login', Controllers\LoginController::class, 'login');
-        $app->get('/home', Controllers\HomeController::class, 'show');
-        $app->get('/logout', Controllers\LogoutController::class, 'logout');
-        $app->get('/bookshelf', Controllers\BookshelfController::class, 'show');
-        $app->post('/bookshelf', Controllers\BookshelfController::class, 'create');
-        $app->post('/bookshelf/delete', Controllers\BookshelfController::class, 'delete');
-        $app->post('/bookshelf/update', Controllers\BookshelfController::class, 'update');
-        $app->get('/books', Controllers\BookController::class, 'show');
+        $router->get('/signup', Controllers\SignupController::class, 'show');
+        $router->post('/signup', Controllers\SignupController::class, 'register');
+        $router->get('/login', Controllers\LoginController::class, 'show');
+        $router->post('/login', Controllers\LoginController::class, 'login');
+        $router->get('/home', Controllers\HomeController::class, 'show');
+        $router->get('/logout', Controllers\LogoutController::class, 'logout');
+        $router->get('/bookshelf', Controllers\BookshelfController::class, 'show');
+        $router->post('/bookshelf', Controllers\BookshelfController::class, 'create');
+        $router->post('/bookshelf/delete', Controllers\BookshelfController::class, 'delete');
+        $router->post('/bookshelf/update', Controllers\BookshelfController::class, 'update');
+        $router->get('/books', Controllers\BookController::class, 'show');
+        $router->get('/bookshelf/{id}', Controllers\BookController::class, 'single');
     }
 }
